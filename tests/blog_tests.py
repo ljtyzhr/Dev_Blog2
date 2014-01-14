7e373a39c78eb85ea2f2acdaae574cdcb850cae4
ScenK <he.kang@dev-engine.com>
2014-01-14 16:00:41 +0800

    move folder into module

---
+++ tests/blog_tests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#import os
+#import dev_blog
+import unittest
+#import tempfile
+
+
+class FlaskrTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
--- tests/test_diary.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-import unittest
-
-
-class TestDiary(unittest.TestCase):
-    pass
-
-if __name__ == '__main__':
-    unittest.main()
