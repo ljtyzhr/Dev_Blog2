45f77160923cc88ae279bdf071a72d4a41a32ec8
He.Kang <he.kang@dev-engine.com>
2014-01-27 15:02:11 +0800

    update bootstrap admin version

--- blog/admin/static/js/backend.js
+++ blog/admin/static/js/backend.js
@@ -1,6 +1,6 @@
 document.write(unescape("%3Cscript src='/admin/static/lib/jquery-2.0.3.min.js' type='text/javascript'%3E%3C/script%3E"));
 document.write(unescape("%3Cscript src='/admin/static/lib/bootstrap/js/bootstrap.min.js' type='text/javascript'%3E%3C/script%3E"));
-document.write(unescape("%3Cscript src='/admin/static/lib/modernizr-2.6.2-respond-1.1.0.min.js' type='text/javascript'%3E%3C/script%3E"));
+document.write(unescape("%3Cscript src='/admin/static/lib/modernizr-build.min.js' type='text/javascript'%3E%3C/script%3E"));
 document.write(unescape("%3Cscript src='/admin/static/lib/datatables/jquery.dataTables.js' type='text/javascript'%3E%3C/script%3E"));
 document.write(unescape("%3Cscript src='/admin/static/lib/datatables/DT_bootstrap.js' type='text/javascript'%3E%3C/script%3E"));
 
--- blog/admin/static/js/backend/main.js
+++ blog/admin/static/js/backend/main.js
@@ -1,43 +1,91 @@
 /**
-* Bootstrap-Admin-Template by onokumus@gmail.com
-* Version : 2.0.0 
-* Author : Osman Nuri Okumuş 
-* Copyright 2013
+* Bootstrap-Admin-Template v2.1.3
+* Author : [object Object] 
+* Copyright 2014
+* Licensed under ,, (,,)
 */
-$(function() {
+$(function () {
     "use strict";
 
-    $('a[href=#]').on('click', function(e){
-      e.preventDefault();
+    $('a[href=#]').on('click', function (e) {
+        e.preventDefault();
     });
-    
-    
+
+
     $('a[data-toggle=tooltip]').tooltip();
     $('a[data-tooltip=tooltip]').tooltip();
 
 
-    $('.minimize-box').on('click', function(e) {
-        e.preventdefault();
+    $('.minimize-box').on('click', function (e) {
+        e.preventDefault();
         var $icon = $(this).children('i');
-        if ($icon.hasclass('icon-chevron-down')) {
-            $icon.removeclass('icon-chevron-down').addclass('icon-chevron-up');
-        } else if ($icon.hasclass('icon-chevron-up')) {
-            $icon.removeclass('icon-chevron-up').addclass('icon-chevron-down');
+        if ($icon.hasClass('icon-chevron-down')) {
+            $icon.removeClass('icon-chevron-down').addClass('icon-chevron-up');
+        } else if ($icon.hasClass('icon-chevron-up')) {
+            $icon.removeClass('icon-chevron-up').addClass('icon-chevron-down');
         }
     });
-    
-    $('.close-box').click(function() {
+    $('.close-box').click(function () {
         $(this).closest('.box').hide('slow');
     });
 
-    $('#changesidebarpos').on('click', function(e) {
-        $('body').toggleclass('hide-sidebar');
+    $('#changeSidebarPos').on('click', function (e) {
+        $('body').toggleClass('hide-sidebar');
+    });
+
+    $('li.accordion-group > a').on('click', function (e) {
+        $(this).children('span').children('i').toggleClass('icon-angle-down');
     });
     
-    $('li.accordion-group > a').on('click',function(e){
-        $(this).children('span').children('i').toggleclass('icon-angle-down');
-    });
+    $('#menu').metisMenu();
+    
 });
+
+;(function ($, window, document, undefined) {
+
+    var pluginName = "metisMenu",
+        defaults = {
+            toggle: true
+        };
+        
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    Plugin.prototype = {
+        init: function () {
+
+            var $this = $(this.element),
+                $toggle = this.settings.toggle;
+
+            $this.find('li.active').has('ul').children('ul').addClass('collapse in');
+            $this.find('li').not('.active').has('ul').children('ul').addClass('collapse');
+
+            $this.find('li').has('ul').children('a').on('click', function (e) {
+                e.preventDefault();
+
+                $(this).parent('li').toggleClass('active').children('ul').collapse('toggle');
+
+                if ($toggle) {
+                    $(this).parent('li').siblings().removeClass('active').children('ul.in').collapse('hide');
+                }
+            });
+        }
+    };
+
+    $.fn[ pluginName ] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, "plugin_" + pluginName)) {
+                $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+            }
+        });
+    };
+
+})(jQuery, window, document);
 
 function metisTable() {
     "use strict";
@@ -52,5 +100,4 @@ function metisTable() {
         }
     });
     /*----------- END datatable CODE -------------------------*/
-
 }
